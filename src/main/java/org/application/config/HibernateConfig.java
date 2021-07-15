3d39972d17ecddbbdba940a22803d9ab470e74d7
Leonid Seniukov <mr.senukov@mail.ru>
2021-07-15 11:54:13 +0300

    fix hibernate/flyway conflict

--- src/main/java/org/application/config/HibernateConfig.java
+++ src/main/java/org/application/config/HibernateConfig.java
@@ -1,7 +1,5 @@
 package org.application.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -12,34 +10,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration
 @EnableTransactionManagement
 public class HibernateConfig {
-
-    private final ApplicationContext context;
-
-    @Autowired
-    public HibernateConfig(ApplicationContext context) {
-        this.context = context;
-    }
-
-//    @Bean
-//    public LocalSessionFactoryBean sessionFactory() {
-//
-//        LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
-//
-//        factoryBean.setConfigLocation(context.getResource("classpath:hibernate.cfg.xml"));
-//        factoryBean.setAnnotatedClasses(UserData.class);
-//
-//        return factoryBean;
-//    }
-//
-//    @Bean
-//    public HibernateTransactionManager transactionManager() {
-//
-//        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-//
-//        transactionManager.setSessionFactory(sessionFactory().getObject());
-//
-//        return transactionManager;
-//    }
 
     @Bean
     public LocalEntityManagerFactoryBean entityManagerFactory() {
